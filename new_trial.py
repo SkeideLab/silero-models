1c9c89e9817bc796277aa646c047680fdc80a47d
ShreyaKapoor18 <kapoor@cbs.mpg.de>
2022-02-21 13:14:28 +0100

    new model trial -- bad results

---
+++ new_trial.py
@@ -0,0 +1,65 @@
+from omegaconf import OmegaConf
+import onnxruntime
+import onnx
+import torch
+import zipfile
+import torchaudio
+from glob import glob
+
+sound_path = '/u/skapoor/COSMIC_data/Folge_1/001_034_4260229664001_DEUE22057786.wav'
+# gpu also works, but our models are fast enough for CPU
+device = torch.device('cpu')
+model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                       model='silero_stt',
+                                       language='de',  # also available 'de', 'es'
+                                       device=device)
+(read_batch, split_into_batches,
+ read_audio, prepare_model_input) = utils  # see function signature for details
+
+# download a single file, any format compatible with TorchAudio
+
+test_files = glob(sound_path)
+batches = split_into_batches(test_files, batch_size=10)
+input = prepare_model_input(read_batch(batches[0]),
+                            device=device)
+
+output = model(input)
+for example in output:
+    print(decoder(example.cpu()))
+
+# %%
+
+language = 'de'  # also available 'de', 'es'
+
+# load provided utils
+_, decoder, utils = torch.hub.load(
+    repo_or_dir='snakers4/silero-models', model='silero_stt', language=language)
+(read_batch, split_into_batches,
+ read_audio, prepare_model_input) = utils
+
+# see available models
+torch.hub.download_url_to_file(
+    'https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml', 'models.yml')
+models = OmegaConf.load('models.yml')
+available_languages = list(models.stt_models.keys())
+assert language in available_languages
+
+# load the actual ONNX model
+torch.hub.download_url_to_file(
+    models.stt_models.en.latest.onnx, 'model.onnx', progress=True)
+onnx_model = onnx.load('model.onnx')
+onnx.checker.check_model(onnx_model)
+ort_session = onnxruntime.InferenceSession('model.onnx')
+
+# download a single file, any format compatible with TorchAudio
+#torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav', dst ='speech_orig.wav', progress=True)
+test_files = [sound_path]
+batches = split_into_batches(test_files, batch_size=10)
+input = prepare_model_input(read_batch(batches[0]))
+
+# actual onnx inference and decoding
+onnx_input = input.detach().cpu().numpy()
+ort_inputs = {'input': onnx_input}
+ort_outs = ort_session.run(None, ort_inputs)
+decoded = decoder(torch.Tensor(ort_outs[0])[0])
+print(decoded)
